6695160d11d5742bf7f8cc092805d7cc43c6a795
codelink7 <codelink7@gmail.com>
2026-08-22 08:47:43 +0300

    configured Spring  and ran a hello world test run

--- src/main/java/com/controlpoint/task/TaskApplication.java
+++ src/main/java/com/controlpoint/task/TaskApplication.java
@@ -2,12 +2,28 @@ package com.controlpoint.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+
+
+
+
+
+
+@RestController
 @SpringBootApplication
 public class TaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(defaultValue = "Spring") String name) {
+		return "Hello, " + name + "!";
 	}
 
 }
